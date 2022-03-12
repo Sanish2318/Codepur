f7a80bde791f47c12f30f6f5ac48f608e76786a1
Sanish Shrestha <np03s200076@heraldcollege.edu.np>
2022-03-12 20:01:10 +0545

    Day 4 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+//import 'screens/home_page.dart';
+import 'screens/login_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Container(
-            child: Text("Hello"),
-          ),
+        home: LoginPage(),
+        themeMode: ThemeMode.light,
+        theme: ThemeData(
+            fontFamily: GoogleFonts.lato().fontFamily,
+            primarySwatch: Colors.deepOrange,
+            primaryTextTheme: GoogleFonts.latoTextTheme()),
+        darkTheme: ThemeData(
+          brightness: Brightness.dark,
         ),
-      ),
-    );
+        routes: {
+          "/login": (context) => LoginPage(),
+        });
   }
 }
