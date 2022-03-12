b96550b56573e30146cc87c9241ba8e02c6d8613
Sanish Shrestha <np03s200076@heraldcollege.edu.np>
2022-03-12 21:19:25 +0545

    Day5 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:film_crew_nepal/screens/home_page.dart';
+import 'package:film_crew_nepal/utils/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-//import 'screens/home_page.dart';
+import 'screens/home_page.dart';
 import 'screens/login_page.dart';
 
 void main() {
@@ -22,7 +24,8 @@ class MyApp extends StatelessWidget {
           brightness: Brightness.dark,
         ),
         routes: {
-          "/login": (context) => LoginPage(),
+          MyRoutes.loginRoute: (context) => LoginPage(),
+          MyRoutes.homeRoute: (context) => HomePage(),
         });
   }
 }
--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:film_crew_nepal/utils/routes.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -5,47 +6,52 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Material(
       color: Colors.white,
-      child: Column(
-        children: [
-          Image.asset('assets/images/login_image.png', fit: BoxFit.cover),
-          SizedBox(
-            height: 20.0,
-          ),
-          Text(
-            "Welcome",
-            style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    hintText: 'Enter Username',
-                    labelText: "Username",
-                  ),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: 'Enter Paswword',
-                    labelText: "Password",
-                  ),
-                ),
-                SizedBox(
-                  height: 20.0,
-                ),
-                ElevatedButton(
-                  child: Text("Login"),
-                  style: TextButton.styleFrom(),
-                  onPressed: () {
-                    print("Hi Coddeysingh");
-                  },
-                ),
-              ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset('assets/images/login_image.png', fit: BoxFit.cover),
+            SizedBox(
+              height: 60.0,
             ),
-          )
-        ],
+            Text(
+              "Welcome",
+              style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Enter Username',
+                      labelText: "Username",
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Enter Paswword',
+                      labelText: "Password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 50.0,
+                  ),
+                  ElevatedButton(
+                    child: Text("Login"),
+                    style: TextButton.styleFrom(
+                      minimumSize: Size(120, 50),
+                    ),
+                    onPressed: () {
+                      Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    },
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
